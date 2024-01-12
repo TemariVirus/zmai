dce48dab9fc7073a612a47d4c46d8a92f2f09af1
Zemogus <kiyonnxperiaz@gmail.com>
2024-01-13 01:57:57 +0800

    Add backpropagation and training loop

--- src/layers.zig
+++ src/layers.zig
@@ -13,15 +13,72 @@ pub const LayerTag = enum {
 pub const Layer = union(LayerTag) {
     dense: Dense,
 
+    /// The number of input neurons.
+    pub fn inputSize(self: Layer) usize {
+        return switch (self) {
+            .dense => |layer| layer.inputSize(),
+        };
+    }
+
+    /// The number of output neurons.
+    pub fn outputSize(self: Layer) usize {
+        return switch (self) {
+            .dense => |layer| layer.outputSize(),
+        };
+    }
+
+    /// The number of trainable parameters.
+    pub fn size(self: Layer) usize {
+        return switch (self) {
+            .dense => |layer| layer.size(),
+        };
+    }
+
+    /// Does a forward pass and stores the activations in `output`.
     pub fn forward(self: Layer, input: []const f32, output: []f32) void {
         switch (self) {
             .dense => |layer| layer.forward(input, output),
         }
     }
 
-    pub fn outputSize(self: Layer) usize {
-        return switch (self) {
-            .dense => |layer| layer.outputSize(),
-        };
+    /// Does a backwards pass and updates the weights and biases of this layer.
+    ///
+    /// `learning_rate` - the learning rate to use.
+    ///
+    /// `input` - the input to this layer in the forward pass. This function
+    /// updates this slice to be the gradient of the loss function with respect to
+    /// the input of this layer.
+    ///
+    /// `output` - the activations of this layer in the forward pass. This function
+    /// updates this slice to be the gradient of the activations with respect to
+    /// the unactivated output of this layer.
+    ///
+    /// `output_grad` - the gradient of the loss function with respect to the
+    /// activations of this layer.
+    ///
+    /// `deltas` - the deltas to add to the weights and biases of this layer. This
+    /// function updates this slice accordingly, but does not apply the deltas.
+    pub fn backward(
+        self: Layer,
+        input: []f32,
+        output: []f32,
+        output_grad: []const f32,
+        deltas: []f32,
+    ) void {
+        switch (self) {
+            .dense => |layer| layer.backward(
+                input,
+                output,
+                output_grad,
+                deltas,
+            ),
+        }
+    }
+
+    /// Updates the weights and biases of this layer.
+    pub fn update(self: Layer, deltas: []const f32, learning_rate: f32) void {
+        switch (self) {
+            .dense => |layer| layer.update(deltas, learning_rate),
+        }
     }
 };
--- src/losses.zig
+++ src/losses.zig
@@ -6,23 +6,23 @@ pub const Loss = enum {
     mean_absolute_error,
     mean_squared_error,
     cross_entropy,
+
+    pub fn forward(loss: Loss, y_pred: []const f32, y_true: []const f32) f32 {
+        return switch (loss) {
+            .mean_absolute_error => mae(y_pred, y_true),
+            .mean_squared_error => mse(y_pred, y_true),
+            .cross_entropy => crossEntropy(y_pred, y_true),
+        };
+    }
+
+    pub fn backward(loss: Loss, y_pred: []f32, y_true: []const f32) void {
+        return switch (loss) {
+            .mean_absolute_error => maeBackward(y_pred, y_true),
+            .mean_squared_error => mseBackward(y_pred, y_true),
+            .cross_entropy => crossEntropyBackward(y_pred, y_true),
+        };
+    }
 };
-
-pub fn forward(loss: Loss, y_pred: []const f32, y_true: []const f32) f32 {
-    return switch (loss) {
-        .mean_absolute_error => mae(y_pred, y_true),
-        .mean_squared_error => mse(y_pred, y_true),
-        .cross_entropy => crossEntropy(y_pred, y_true),
-    };
-}
-
-pub fn backward(loss: Loss, y_pred: []const f32, y_true: []f32) void {
-    return switch (loss) {
-        .mean_absolute_error => maeBackward(y_pred, y_true),
-        .mean_squared_error => mseBackward(y_pred, y_true),
-        .cross_entropy => crossEntropyBackward(y_pred, y_true),
-    };
-}
 
 pub fn mae(y_pred: []const f32, y_true: []const f32) f32 {
     assert(y_pred.len == y_true.len);
@@ -34,7 +34,7 @@ pub fn mae(y_pred: []const f32, y_true: []const f32) f32 {
     return sum / @as(f32, @floatFromInt(y_true.len));
 }
 
-pub fn maeBackward(y_pred: []f32, y_true: []f32) void {
+pub fn maeBackward(y_pred: []f32, y_true: []const f32) void {
     assert(y_pred.len == y_true.len);
 
     for (0..y_true.len) |i| {
@@ -52,7 +52,7 @@ pub fn mse(y_pred: []const f32, y_true: []const f32) f32 {
     return sum / @as(f32, @floatFromInt(y_true.len));
 }
 
-pub fn mseBackward(y_pred: []f32, y_true: []f32) void {
+pub fn mseBackward(y_pred: []f32, y_true: []const f32) void {
     assert(y_pred.len == y_true.len);
 
     for (0..y_true.len) |i| {
@@ -70,7 +70,7 @@ pub fn crossEntropy(y_pred: []const f32, y_true: []const f32) f32 {
     return sum;
 }
 
-pub fn crossEntropyBackward(y_pred: []f32, y_true: []f32) void {
+pub fn crossEntropyBackward(y_pred: []f32, y_true: []const f32) void {
     assert(y_pred.len == y_true.len);
 
     for (0..y_true.len) |i| {
